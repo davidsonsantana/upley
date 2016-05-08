83d01e28c6e1dbd8d2658d29ff4de65b840374e6
Davidson Santana <davidsonjsantana@gmail.com>
2016-05-08 00:29:28 -0300

    Finalizando a home e estrutura básica inicial

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,23 @@
 $(function(){
 
-  
+	var userFeed = new Instafeed({
+		get: 'user',
+		userId: 421500595,
+		accessToken: '174534218.44724b7.4031c040a5cd48089a25759d46801e9a',
+		template: '<li class="col-md-3 col-sm-4 col-xs-6"><a href="{{image}}"><img class="img-responsive img-thumbnail" src="{{image}}" /></a></li>',
+		resolution: 'standard_resolution',
+		limit: '12',
+		after: function(){
+			$('#instafeed li a').colorbox({
+				rel: 'galeria',
+				maxWidth: '80%',
+				maxHeight: '90%',
+				previous: 'Foto Anterior',
+				next: 'Próxima Foto',
+				close: 'Fechar'
+			});
+		}
+	});
+	userFeed.run();
   
 });
